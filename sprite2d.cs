2c50e5e917e3ca82464e702c0ea8367de7b3cbf9
systemerror <sys_temerror@web.de>
2024-11-04 10:30:14 +0100

    Add files via upload

---
+++ sprite2d.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+using vblibrary;
+
+public partial class sprite2d : Sprite2D
+{
+    Class1 vbclass = new Class1();
+    
+	// Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+		vbclass._Ready(this);	
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		vbclass._Process(delta);
+	}
+}
--- vblibrary/Class1.vb
+++ vblibrary/Class1.vb
@@ -1,12 +1,24 @@
+
 Imports Godot
 
 Public Class Class1
-    Public Sub _Ready()
+
+    Private timepassed = 0
+    Private sprite1 As Sprite2D
+
+    Public Sub _Ready(ByVal sprite As Sprite2D)
         GD.Print("hello world from visualbasic")
+        sprite1 = sprite
     End Sub
 
-    Public Sub _Process()
+    Public Sub _Process(ByVal delta As Double)
+        timepassed += delta
 
+        Dim newposition As Vector2
+        newposition.X = 200.0 + (10.0 * Math.Sin(timepassed * 2.0))
+        newposition.Y = 200.0 + (10.0 * Math.Cos(timepassed * 1.5))
+
+        sprite1.Position = newposition
     End Sub
 
 End Class
